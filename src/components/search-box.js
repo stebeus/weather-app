95b0a376dca4f1ff6f1a953d2cfad52a34ae5703
stebeus <189894010+stebeus@users.noreply.github.com>
2026-01-20 17:00:59 -0300

    fix: prevent the default event

--- src/components/search-box.js
+++ src/components/search-box.js
@@ -2,7 +2,8 @@ import { fetchTimeline } from "../services/weatherService";
 
 const inputQuery = document.querySelector("#query");
 
-export function handleSearch() {
+export function handleSearch(event) {
+  event.preventDefault();
   fetchTimeline(inputQuery.value);
   console.log(1);
 }
