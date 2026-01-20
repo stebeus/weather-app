a76d96230fe5b9a605527544e31adb220b19e8bd
stebeus <189894010+stebeus@users.noreply.github.com>
2026-01-20 17:25:21 -0300

    feat: create forecast render

--- src/components/forecast.js
+++ src/components/forecast.js
@@ -1,1 +1,14 @@
 const main = document.querySelector("main");
+
+export function renderForecast(object) {
+  const template = document.querySelector("#forecast-template");
+  const clone = template.content.cloneNode(true);
+
+  clone.querySelector("#title").textContent = object[0];
+  clone.querySelector("#temperature").textContent = object[1];
+  clone.querySelector("#feelslike").textContent = object[2];
+  clone.querySelector("#humidity").textContent = object[3];
+  clone.querySelector("#description").textContent = object[4];
+
+  main.appendChild(clone);
+}
