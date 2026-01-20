2b0b31777e23fb494a81f72cbbd1b24d75730ebe
stebeus <189894010+stebeus@users.noreply.github.com>
2026-01-20 17:40:29 -0300

    fix: use the destructured object keys for the text contents

--- src/components/forecast.js
+++ src/components/forecast.js
@@ -1,14 +1,20 @@
 const main = document.querySelector("main");
 
-export function renderForecast(object) {
+function renderForecast({
+  resolvedAddress,
+  temp,
+  feelslike,
+  humidity,
+  description,
+}) {
   const template = document.querySelector("#forecast-template");
   const clone = template.content.cloneNode(true);
 
-  clone.querySelector("#title").textContent = object[0];
-  clone.querySelector("#temperature").textContent = object[1];
-  clone.querySelector("#feelslike").textContent = object[2];
-  clone.querySelector("#humidity").textContent = object[3];
-  clone.querySelector("#description").textContent = object[4];
+  clone.querySelector("#title").textContent = resolvedAddress;
+  clone.querySelector("#temperature").textContent = temp;
+  clone.querySelector("#feelslike").textContent = feelslike;
+  clone.querySelector("#humidity").textContent = humidity;
+  clone.querySelector("#description").textContent = description;
 
   main.appendChild(clone);
 }
