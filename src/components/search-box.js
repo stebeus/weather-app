b69450e91e327f717ea7572070b5efe0551060ab
stebeus <189894010+stebeus@users.noreply.github.com>
2026-01-20 16:54:25 -0300

    feat: create search handler

--- src/components/search-box.js
+++ src/components/search-box.js
@@ -2,3 +2,8 @@ import { fetchTimeline } from "../services/weatherService";
 
 const searchBox = document.querySelector(".search-box");
 const inputQuery = document.querySelector("#query");
+
+export function handleSearch() {
+  fetchTimeline(inputQuery.value);
+  console.log(1);
+}
