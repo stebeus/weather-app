6be238516384408448f6c136aa81098fab081465
stebeus <189894010+stebeus@users.noreply.github.com>
2026-01-20 16:54:46 -0300

    feat: remove search-box constant

--- src/components/search-box.js
+++ src/components/search-box.js
@@ -1,6 +1,5 @@
 import { fetchTimeline } from "../services/weatherService";
 
-const searchBox = document.querySelector(".search-box");
 const inputQuery = document.querySelector("#query");
 
 export function handleSearch() {
