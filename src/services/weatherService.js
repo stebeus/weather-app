ef8dfb16d4aaceff0a48d40d52d1faaffabc2d4e
stebeus <189894010+stebeus@users.noreply.github.com>
2026-01-20 15:16:57 -0300

    feat: create fetchTimeline function

--- src/services/weatherService.js
+++ src/services/weatherService.js
@@ -1,1 +1,17 @@
-export {};
+async function fetchTimeline(location) {
+  const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=XT52FMHNWKEWVE7B5XQZR4MGY`;
+
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Request failed with status: ${response.status}`);
+    }
+
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+export { fetchTimeline };
