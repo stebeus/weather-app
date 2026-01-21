b8d34c1677226799fc4bf4fc9cd562479021fcf1
stebeus <189894010+stebeus@users.noreply.github.com>
2026-01-21 13:02:17 -0300

    feat: query select switch-unit toggle

--- src/main.js
+++ src/main.js
@@ -3,5 +3,6 @@ import "/favicon.png";
 import { handleSearch } from "./components/search";
 
 const searchBox = document.querySelector(".search-box");
+const unitToggleSwitch = document.querySelector("[data-action='switch-unit']");
 
 searchBox.addEventListener("submit", handleSearch);
