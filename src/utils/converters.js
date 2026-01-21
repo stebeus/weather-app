38b5e5145d942d79ea5a868e23094bfadd8b9b2f
stebeus <189894010+stebeus@users.noreply.github.com>
2026-01-21 09:01:17 -0300

    feat: create temperature rounder

--- src/utils/converters.js
+++ src/utils/converters.js
@@ -1,4 +1,9 @@
-const convertToFahrenheit = (temperature) => temperature * (9 / 5) + 32;
-const convertToCelsius = (temperature) => (temperature - 32) / (9 / 5);
+const roundTemperature = (temperature) => Math.round(temperature);
+
+const convertToFahrenheit = (temperature) =>
+  roundTemperature(temperature * (9 / 5) + 32);
+
+const convertToCelsius = (temperature) =>
+  roundTemperature((temperature - 32) / (9 / 5));
 
 export { convertToFahrenheit, convertToCelsius };
