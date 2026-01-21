6642430ffcabfce1e62ce27f8d0e7b61d04bfc07
stebeus <189894010+stebeus@users.noreply.github.com>
2026-01-21 13:02:43 -0300

    feat: import switchUnit

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import "./style.css";
 import "/favicon.png";
 import { handleSearch } from "./components/search";
+import { switchUnit } from "./components/unit-switcher";
 
 const searchBox = document.querySelector(".search-box");
 const unitToggleSwitch = document.querySelector("[data-action='switch-unit']");
