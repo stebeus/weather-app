ca430379aab868fcec1d5b52e925c0cc476753ce
stebeus <189894010+stebeus@users.noreply.github.com>
2026-01-21 13:57:28 -0300

    feat: create unitSwitchToggler handler

--- src/components/unit-switcher.js
+++ src/components/unit-switcher.js
@@ -7,3 +7,22 @@ let currentUnit = "us";
 const toggleCelsius = () => (isCelsiusToggled = !isCelsiusToggled);
 const switchCurrentUnit = () =>
   (currentUnit = currentUnit === "us" ? "metric" : "us");
+
+export function handleUnitSwitchToggler() {
+  const ids = ["temperature", "feels-like"];
+
+  toggleCelsius();
+  switchCurrentUnit();
+
+  for (const id of ids) {
+    const element = document.getElementById(id);
+
+    if (isCelsiusToggled) {
+      element.dataset.value = convertToCelsius(element.dataset.value);
+      element.textContent = formatTemperature(element.dataset.value, "C");
+    } else {
+      element.dataset.value = convertToFahrenheit(element.dataset.value);
+      element.textContent = formatTemperature(element.dataset.value);
+    }
+  }
+}
