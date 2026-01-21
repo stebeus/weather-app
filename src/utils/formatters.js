e7cd25fe7582e8f95e87a0517e167eff8e1175f8
stebeus <189894010+stebeus@users.noreply.github.com>
2026-01-21 07:32:28 -0300

    feat: create location formatter

--- src/utils/formatters.js
+++ src/utils/formatters.js
@@ -1,2 +1,7 @@
 const capitalize = (string) =>
   string[0].toUpperCase(0) + string.slice(1).toLowerCase();
+
+const formatLocation = (location) =>
+  location.split(" ").map(capitalize).join(" ");
+
+export { formatLocation };
