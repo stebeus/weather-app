86c6038f5f88f3dee593da15256bc5b2fbc50784
stebeus <189894010+stebeus@users.noreply.github.com>
2026-01-21 13:51:02 -0300

    feat: create currentUnit variable

--- src/components/unit-switcher.js
+++ src/components/unit-switcher.js
@@ -2,3 +2,4 @@ import { convertToFahrenheit, convertToCelsius } from "../utils/converters";
 import { formatTemperature } from "../utils/formatters";
 
 let isCelsiusToggled = true;
+let currentUnit = "us";
