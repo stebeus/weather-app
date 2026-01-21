a93e2c8b2e192ad66474098c923c2ae693410653
stebeus <189894010+stebeus@users.noreply.github.com>
2026-01-21 09:42:26 -0300

    feat: create toggleSwitch handler

--- src/components/unit-switcher.js
+++ src/components/unit-switcher.js
@@ -1,2 +1,7 @@
 import { convertToFahrenheit, convertToCelsius } from "../utils/converters";
 import { formatTemperature } from "../utils/formatters";
+
+function handleToggleSwitch(event, truthyStatement, falsyStatement) {
+  const toggleSwitch = event.target.closest("[type='checkbox");
+  toggleSwitch.checked ? truthyStatement : falsyStatement;
+}
