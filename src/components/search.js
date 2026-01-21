6ffcfa93f953e5027442c13a09651386e2c7c9c6
stebeus <189894010+stebeus@users.noreply.github.com>
2026-01-21 14:20:07 -0300

    feat: import currentUnitQuery

--- src/components/search.js
+++ src/components/search.js
@@ -1,8 +1,9 @@
 import { renderFetchedTimeline } from "./display";
+import { currentUnitQuery } from "./unit-switcher";
 
 const inputQuery = document.querySelector("#query");
 
 export function handleSearch(event) {
   event.preventDefault();
-  renderFetchedTimeline(inputQuery.value);
+  renderFetchedTimeline(inputQuery.value, currentUnitQuery);
 }
