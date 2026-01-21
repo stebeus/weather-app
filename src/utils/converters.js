9c2bd18298851ddaad743eb20287d42af5e7d000
stebeus <189894010+stebeus@users.noreply.github.com>
2026-01-21 08:55:33 -0300

    feat: create fahrenheit converter

--- src/utils/converters.js
+++ src/utils/converters.js
@@ -1,3 +1,4 @@
+const convertToFahrenheit = (temperature) => temperature * (9 / 5) + 32;
 const convertToCelsius = (temperature) => (temperature - 32) / (9 / 5);
 
-export { convertToCelsius };
+export { convertToFahrenheit, convertToCelsius };
