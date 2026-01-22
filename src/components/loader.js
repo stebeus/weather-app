4e142166a994d4b4d5b83a4f4eddc0ed0f12d7cb
stebeus <189894010+stebeus@users.noreply.github.com>
2026-01-22 07:45:18 -0300

    refactor: return only the clone in loader render

--- src/components/loader.js
+++ src/components/loader.js
@@ -1,5 +1,5 @@
 export function renderLoader() {
   const template = document.querySelector("#loader-template");
   const clone = template.content.cloneNode(true);
-  return clone.querySelector(".loader");
+  return clone;
 }
